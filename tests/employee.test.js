861a639241b0ce5e1294b5ec921a1147b0262128
Sadie Sial <sadiesial@gmail.com>
2021-10-04 20:46:50 -0500

    created variables in app

---
+++ tests/employee.test.js
@@ -0,0 +1,9 @@
+// instal node_modules with npm init, npm i, npm inquirer, + npm fs
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// create constructor classes (using js files from .lib/)
+const Manager = require('./lib/manager')
+const Engineer = require('./lib/engineer')
+const Intern = require('./lib/intern')
+
