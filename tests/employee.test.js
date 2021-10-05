c1394cf1b0f7d8ae3e4e24817691817c48502651
Sadie Sial <sadiesial@gmail.com>
2021-10-04 21:17:01 -0500

    cont. declaring my vars

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -3,7 +3,15 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // create constructor classes (using js files from .lib/)
-const Manager = require('./lib/manager')
-const Engineer = require('./lib/engineer')
-const Intern = require('./lib/intern')
+const Manager = require('./lib/manager');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
+
+// variable for generating the HTML product
+const generateHTML = require('./src/generateHTML');
+
+// creating team variable to encapsulate inside generateHTML()
+const myTeam = [];
+
+// ask inquirer about: 1. manager 
 
