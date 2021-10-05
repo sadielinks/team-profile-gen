f107b6a3a9cc52f1d29d6a38770f8eb123fa5fb8
Sadie Sial <sadiesial@gmail.com>
2021-10-04 21:50:24 -0500

    tutor noticed that I wrote in the test file WHOOOPSgit add .! copied code into true index.js file

--- index.js
+++ index.js
@@ -1,1 +1,125 @@
-// main app file
+// instal node_modules with npm init, npm i, npm inquirer, + npm fs
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+// create constructor classes (using js files from .lib/)
+const Manager = require("./lib/manager");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
+
+// variable for generating the HTML product
+const generateHTML = require("./src/generateHTML");
+
+// creating team variable to encapsulate inside generateHTML()
+const myTeam = [];
+
+// ask inquirer: 1. manager
+// async function so that promise can be used
+const manager = async () => {
+  const managerQuestions = [
+    {
+      type: "input",
+      name: "name",
+      message: "Hi there, please enter the manager's name:",
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "What is the manager's ID?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is the manager's email?",
+    },
+    {
+      type: "input",
+      name: "office",
+      message: "What is the manager's office number?",
+    },
+  ];
+
+  // use await for promise
+  const { name, id, email, office } = await inquirer.prompt(managerQuestions);
+
+  // create variable for manger from class & user prompt answers
+  const manager = new Manager(name, id, email, office);
+  team.push(manager);
+
+  // call js file from another js file with 'another'
+  another();
+};
+
+// ask inquirer: 2. engingeer
+const engineer = async () => {
+    const engineerQuestions = [
+      {
+        type: "input",
+        name: "name",
+        message: "Hi there, please enter an engingeer's name:",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is this engineer's ID?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is this engineers's email?",
+      },
+      {
+        type: "input",
+        name: "GitHub",
+        message: "What is this engineer's GitHub?",
+      },
+    ];
+  
+    // use await for promise
+    const { name, id, email, GitHub } = await inquirer.prompt(engineerQuestions);
+  
+    // create variable for manger from class & user prompt answers
+    const engineer = new Engineer(name, id, email, GitHub);
+    team.push(engineer);
+  
+    // call js file from another js file with 'another'
+    another();
+  };
+
+  // ask inquirer: 3. intern
+  const intern = async () => {
+    const internQuestions = [
+      {
+        type: "input",
+        name: "name",
+        message: "Hi there, please enter the intern's name:",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is this intern's ID?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is this intern's email?",
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "What school does this intern attend?",
+      },
+    ];
+  
+    // use await for promise
+    const { name, id, email, school } = await inquirer.prompt(internQuestions);
+  
+    // create variable for manger from class & user prompt answers
+    const intern = new Intern(name, id, email, school);
+    team.push(intern);
+  
+    // call js file from another js file with 'another'
+    another();
+  };
+
+  
