8410a4534e90cbf5457a3b6e7d7a074c309000ce
Sadie Sial <sadiesial@gmail.com>
2021-10-09 23:36:36 -0500

    added manager to employeeHTML.js

--- dist/employeeHTML.js
+++ dist/employeeHTML.js
@@ -49,4 +49,27 @@ const generateTeamHTML = (myTeam) => {
 </html>`;
 };
 
+// manager
+const gimmeManager = (manager) => {
+  return
+  `<div class="card employee-card m-3 shadow-lg col-3">
+  <div class="card-header bg-info text-white">
+      <h2 class="card-title">${manager.name}</h2>
+      <h3 class="card-title"><i class="fas fa-chalkboard-teacher"></i>/h3>
+  </div>
+  <div class="card-body bg-light">
+      <ul class="list-group font-weight-bold">
+          <li class="list-group-item">ID: ${manager.id}</li>
+          <li class="list-group-item">Email: ${manager.email}<a href="mailto:{{ email }}">{{ email }}</a></li>
+          <li class="list-group-item">Office Number: ${manager.officeNumber}</li>
+      </ul>
+  </div>
+</div>`;
+};
+
+// engineer(s)
+
+
+// intern(s)
+
 module.exports = generateTeamHTML;
