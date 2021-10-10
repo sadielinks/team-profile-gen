efec0061a5c5035752c65793bdd96cdeda419f97
Sadie Sial <sadiesial@gmail.com>
2021-10-09 19:36:45 -0500

    hashed out employee test

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -6,12 +6,25 @@ const Employee = require('../lib/employee');
 test("Name Test", () => {
     const testValue = "Kobe";
     const nameTest = new Employee(testValue);
+    // getName from employee.js
     expect(nameTest.getName()).toBe(testValue);
   });
 
 
 // id testing
-test('Print Employee Id')
+test("Id Test", () => {
+    const testValue = "24";
+    // add previous (name) value with this new testValue
+    const idTest = new Employee("Kobe", testValue);
+    // getId from employee.js
+    expect(idTest.getId()).toBe(testValue);
+  });
 
 // email testing
-test('Print Employee Email')
+test("Email Test", () => {
+    const testValue = "lakers@fakemail.com";
+    // add previous (name + id) values with this new testValue
+    const idTest = new Employee('Kobe', '24', testValue);
+    // getId from employee.js
+    expect(idTest.getEmail()).toBe(testValue);
+  });
