ab85201dcf2d915cec55b25d9bab5b29b9803f8f
Sadie Sial <sadiesial@gmail.com>
2021-10-09 19:53:45 -0500

    manager test complete with role

--- tests/manager.test.js
+++ tests/manager.test.js
@@ -6,7 +6,13 @@ const Manager = require('../lib/manager');
 test('Office Number Test', () => {
     const testValue = '1';
     // same as employee test, add in values to tack on testing value (1)
-    const officeNumber = new Manager('Kobe', 24, 'lakers@fakegmail', testValue);
+    const officeTest = new Manager('Kobe', 24, 'lakers@fakegmail', testValue);
     // getOfficeNumber from manager.js
-    expect(officeNumber.getOfficeNumber()).toBe(testValue);
+    expect(officeTest.getOfficeNumber()).toBe(testValue);
+});
+
+test('Role Test', () => {
+    const testValue = 'Manager';
+    const roleTest = new Manager('Kobe', 24, 'lakers@fakegmail', 1);
+    expect(roleTest.getRole()).toBe(testValue);
 });
